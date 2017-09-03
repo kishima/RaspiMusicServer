a90ecebabd90cf3f339ff21fbb4b1ade2e8a20a0
kishima <kishima7@gmail.com>
2017-09-03 22:31:29 +0900

    add IR Distance Interrupter sensor

---
+++ audioserver/ap_motion_detect.py
@@ -0,0 +1,16 @@
+﻿import logging
+import time
+import grovepi
+
+class ApMotionDetect:
+	def __init__(self,digital_pin):
+		self.pin = digital_pin
+		grovepi.pinMode(self.pin,"INPUT")
+		return
+	
+	def check_status(self):
+		stat = grovepi.digitalRead(self.pin)
+		if stat == 1:
+			return False
+		else:
+			return True
--- audioserver/music_server.py
+++ audioserver/music_server.py
@@ -8,6 +8,7 @@ import ap_lcd
 import ap_joystick
 import ap_button
 import ap_menu
+import ap_motion_detect
 import arduino_timer
 
 logging.basicConfig(level=logging.DEBUG,format='%(asctime)s (%(threadName)-10s) %(message)s',)
@@ -15,12 +16,21 @@ logging.basicConfig(level=logging.DEBUG,format='%(asctime)s (%(threadName)-10s) 
 timer = arduino_timer.ArduinoTimer()
 timer.set_timer_once()
 
+from pykakasi import kakasi
+kakasi = kakasi()
+kakasi.setMode('H', 'a')
+kakasi.setMode('K', 'a')
+kakasi.setMode('J', 'a')
+conv = kakasi.getConverter()
+print(conv.do(U'本日は晴天なり')) # honjitsuhaseitennari
+
 lcd = ap_lcd.ApLcd()
 lcd.set_bg_rgb(0,50,0)
 lcd.start()
 joystick  = ap_joystick.ApJoystick(pinX=1,pinY=0)
 button    = ap_button.ApButton(pin=6)
 volume    = ap_volume.ApVolume(barPin=2,ledPin=17,display_obj=lcd)
+motion    = ap_motion_detect.ApMotionDetect(digital_pin=3)
 menu = ap_menu.ApMenu(lcd,volume)
 
 cnt = 0
@@ -32,12 +42,9 @@ while True:
 		y=0
 		
 		if 0 == cnt % 4 :
+			print(motion.check_status())
 			volume.volume_check()
-
-		if 0 == cnt % 4:
 			button_stat = button.get_button_stat()
-
-		if 0 == cnt % 4 :
 			x,y = joystick.get_axis()
 
 		volume.led_update(cnt)
