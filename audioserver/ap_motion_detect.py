4d352f75539541613916c7a24ee6118a8b314d25
kishima <kishima7@gmail.com>
2017-09-05 01:00:32 +0900

    change motion detection HW

--- audioserver/ap_motion_detect.py
+++ audioserver/ap_motion_detect.py
@@ -3,22 +3,28 @@ import time
 import grovepi
 from collections import Counter
 from collections import deque
+import RPi.GPIO as gpio
 
 class ApMotionDetect:
-	def __init__(self,digital_pin):
-		self.pin = digital_pin
-		grovepi.pinMode(self.pin,"INPUT")
-		self.history = deque([1 for i in range(20)])
+#	def __init__(self,digital_pin):
+#		self.pin = digital_pin
+#		grovepi.pinMode(self.pin,"INPUT")
+	def __init__(self,gpio_pin):
+		self.pin = gpio_pin
+		gpio.setmode(gpio.BCM)
+		gpio.setup(self.pin ,gpio.IN)
+		self.history = deque([0 for i in range(20)])
 		return
 	
 	def check_status(self):
-		stat = grovepi.digitalRead(self.pin)
+		#stat = grovepi.digitalRead(self.pin)
+		stat = gpio.input(self.pin)
 		self.history.append(stat)
 		self.history.popleft()
-		if stat == 1:
+		if stat == 0:
 			return False
 		else:
 			return True
 	
 	def get_latest_status(self):
-		return self.history.count(0)
+		return self.history.count(1)
